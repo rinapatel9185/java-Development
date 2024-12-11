5b7df91d4358820372478bac0939be473ab56396
rinapatel9185 <patelreena228@gmail.com>
2024-12-11 03:54:23 +0000

    arthmatic operatao

---
+++ Basic/ArithmeticOperators.java
@@ -0,0 +1,26 @@
+package Dec_9_2024;
+
+public class ArithmeticOperators {
+	
+	public static void main(String[] args) {
+		
+		int number1=253;
+		int number2=65;
+		
+		int result;
+		
+		result=number1+number2;
+		
+		System.out.println("Result is:"+result);
+		result=number1-number2;
+		System.out.println("Result is:"+result);
+		result=number1*number2;
+		System.out.println("Result is:"+result);
+		result=number1/number2;
+		System.out.println("Result is:"+result);
+		result=number1%number2;
+		System.out.println("Result is:"+result);
+		
+	}
+
+}
