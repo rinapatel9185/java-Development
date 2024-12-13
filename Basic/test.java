aafe90867ca6c3a329cc72b6769db8971e5f0c56
rinapatel9185 <patelreena228@gmail.com>
2024-12-13 08:42:40 +0530

    Create test.java

---
+++ Basic/test.java
@@ -0,0 +1,17 @@
+package patel;
+
+public class Ifstatement {
+	
+	public static void main(String [] args) {
+		
+		int age = 18;
+        
+		if(age<=18) {
+		
+		System.out.println("YOU ARE ELIGIBLE ");
+		
+		
+		}
+	}
+
+}
