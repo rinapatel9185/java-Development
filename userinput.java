e03ff4f1e3edf3e95d0df39523fd7f269420e334
rinapatel9185 <patelreena228@gmail.com>
2024-12-16 03:48:25 +0000

    switchcase

---
+++ userinput.java
@@ -0,0 +1,21 @@
+package basic;
+
+public class userinput {
+	
+	public static void main(String[] args ) {
+		
+		int n1=10;
+		int n2=25;
+		int n3=96;
+
+		int result ;
+		result= n1+n2+n3;
+		System.out.println("RESULT IS :" +result);
+		
+		
+		
+		
+		
+	}
+
+}
