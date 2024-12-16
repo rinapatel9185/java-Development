8cff7d99fb22d04c88995bf14e6e0aeb14eab64c
rinapatel9185 <patelreena228@gmail.com>
2024-12-16 03:38:54 +0000

    switchcase

---
+++ Basic/switchcase.java
@@ -0,0 +1,36 @@
+package Basic;
+import java.util.Scanner;
+public class switchcase {
+
+    public static void main(String[] args) {
+        
+        // create an object 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Day Number ");
+        int day;
+        day = sc.nextInt();
+
+        switch (day) {
+                case 1 : System.out.println("Monday");          
+                break;
+                case 2 : System.out.println("Tuesday");          
+                break;
+                case 3 : System.out.println("Wednesday");          
+                break;
+                case 4 : System.out.println("Thursday");          
+                break;
+                case 5 : System.out.println("Friday");          
+                break;
+                case 6 : System.out.println("Saturday");          
+                break;
+                case 7 : System.out.println("Sunday");          
+                break;
+            default: System.out.println("Enter Only 1 - 7");
+                break;
+        }
+
+
+
+    }
+    
+}
