2ed31278f9daa4384a4f5564c923ce7650fc88a3
rinapatel9185 <patelreena228@gmail.com>
2024-12-28 08:30:54 +0000

    practictime

---
+++ Day_06_2024/praticeloop.java
@@ -0,0 +1,27 @@
+package practiceloop;
+
+public class praticeloop {
+
+    public static void main(String[] args){
+
+        int i;
+
+        for(i=0;i<=5;i++){
+
+            for(int j=0;j<=5;j++){
+
+             System.out.println("j");
+
+            }
+
+            System.out.println("i");
+
+
+        }
+
+    }
+
+    
+}
+
+
