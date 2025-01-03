6acffdd45fbdd62e922d94fc6df8757dd944bee5
rinapatel9185 <patelreena228@gmail.com>
2025-01-03 03:14:01 +0000

    chararray_03_jan

---
+++ Day_07_2025/array_eg.java
@@ -0,0 +1,27 @@
+package college;
+
+import java.util.Scanner;
+
+public class array_eg {
+
+ public static void main(String [] args){
+
+    char data[] = new char[4];
+
+    data[0]='R';
+    data[1]='I';
+    data[2]='N';
+    data[3]='A';
+
+
+    for(int i=0;i<data.length;i++){
+     System.out.println((i+1)+" = "+data[i]);
+        }
+    }
+ }
+
+
+
+
+
+    
