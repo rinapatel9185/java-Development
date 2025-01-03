8d47552e8f56dfac01a337b4fc12f10bb10a513f
rinapatel9185 <patelreena228@gmail.com>
2025-01-03 03:30:21 +0000

    Stringarray.java

---
+++ Day_07_2025/array_SUM.java
@@ -0,0 +1,28 @@
+package college;
+
+import java.util.Scanner;
+
+public class array_SUM{
+
+ public static void main(String [] args){
+
+    String data[] = new String[4];
+
+    data[0]="Hina";
+    data[1]="Riya";
+    data[2]="Siya";
+    data[3]="Priya";
+
+
+    for(int i=0;i<data.length;i++){
+     System.out.println((i+1)+" = "+data[i]);
+
+        }
+    }
+ }
+
+
+
+
+
+    
