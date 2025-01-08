d1ed4ffe20af3f74daa68641d00ab132eb96fa11
rinapatel9185 <patelreena228@gmail.com>
2025-01-08 02:51:37 +0000

    simple

---
+++ Day_08_6jan/basicfunction.java
@@ -0,0 +1,16 @@
+package basicfunction;
+
+class Basicfunction1(){
+
+    static void info(){
+
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        info();
+    }
+
+}
+  
+
