ce88a1e2209043490f88f2171606b7bba93aa0b8
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-02-03 03:21:45 +0000

    Add Single Inheritance example with Car and Vehicle classes

---
+++ Types_of_Inheritance/Single_Inheritance/Types_of_Inheritance/Single_Inheritance/Car.java
@@ -0,0 +1,13 @@
+package Types_of_Inheritance.Single_Inheritance;
+
+public class Car extends Vehicle {
+
+    public void service() {
+        System.out.println("Car Serviced");
+    }
+
+    public void repair() {
+        System.out.println("Car Repaired");
+    }
+    
+}
---
+++ Types_of_Inheritance/Single_Inheritance/Types_of_Inheritance/Single_Inheritance/SingleInheritanceMain.java
@@ -0,0 +1,15 @@
+package Types_of_Inheritance.Single_Inheritance;
+
+public class SingleInheritanceMain {
+
+    public static void main(String[] args) {
+        
+        Car car = new Car();
+        car.start();
+        car.stop();
+        car.service();
+        car.repair();
+
+    }
+    
+}
