31a20e378ae89c6b3c3eabd5095f5e566111ba45
rinapatel9185 <patelreena228@gmail.com>
2025-02-05 08:28:55 +0530

    Add files via upload

---
+++ inheritance01/Childclass.java
@@ -0,0 +1,11 @@
+package inheritance01;
+
+public class Childclass extends Parentclass{
+	
+	public void bark() {
+		System.out.println("Barking");
+	}
+   public void slow() {
+	   System.out.println("SLOW");
+   }
+}
