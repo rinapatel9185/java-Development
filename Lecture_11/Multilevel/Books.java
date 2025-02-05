d94b9cecb6737c8c4c2c2ef5e01b20cdfac2b68f
Pankaj Chouhan <pankajchouhan80@gmail.com>
2025-02-05 03:12:55 +0000

    Add Books class with method to display book details

---
+++ Lecture_11/Multilevel/Books.java
@@ -0,0 +1,9 @@
+public class Books {
+
+    void books_details(){
+        System.out.println("Book Name: Java Programming");
+        System.out.println("Book Author: John Smith");
+        System.out.println("Book Price: $50.00");
+    }
+    
+}
