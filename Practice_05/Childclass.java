d44a0e3de828e7c5870ee2bc74cb2812f0459ed9
rinapatel9185 <patelreena228@gmail.com>
2025-02-07 09:19:13 +0530

    Add files via upload

---
+++ Practice_05/Childclass.java
@@ -0,0 +1,11 @@
+package inheritance01;
+
+public class Childclass extends Parentclass{
+	
+	public void FAST() {
+		System.out.println("FAST");
+	}
+   public void slow() {
+	   System.out.println("SLOW");
+   }
+}
